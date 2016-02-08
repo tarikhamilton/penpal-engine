ac07894cf753aa3d4628b7c3eb37e6db32a2ddf2
tikitariki <tarikhamilton@gmail.com>
2016-02-08 01:19:28 -0500

    Expanded emoji class

--- src/emoji.js
+++ src/emoji.js
@@ -4,12 +4,56 @@
  * @class Emoji
  * @constructor
  */
-
-var emoji = function (options) {
+var Emoji = function (options) {
+    var options = options || {},
+        required = []
     /**
-     * @property orientation
+     * Whether the emoji moves left-to-right or in all directions.
+     * 
+     * @property movementType
      * @type String
      * @required
      */
-    this.orientation = options.orientation || return 'Not going to fly, emoji.orientation is required.'
+    this.movementType = options.movementType || null
+    
+    /**
+     * Initial facing direction.
+     * 
+     * @property initialFacingDirection
+     * @type String
+     * @required
+     */
+    this.initialFacingDirection = options.initialFacingDirection || null
+    
+    /**
+     * Emoji CSS handle
+     * 
+     * @property img
+     * @type String
+     * @required
+     */
+    this.img = options.img || null
+    
+    // Check if required properties are met
+    
+    required['movementType'] = this.movementType
+    required['initialFacingDirection'] = this.initialFacingDirection
+    required['img'] = this.img
+
+    for (var prop in required) {
+        if (required[prop] === null)
+            console.log(prop + ' is a required property for Emoji.')
+    } 
+    
+    // Create emoji HTML
+    
+    var animal = document.createElement('player'),
+		playerIndicator = document.createElement('div'),
+		img = document.createElement('i')
 }
+
+var test = new Emoji({
+    img: 'em-cat2',
+    movementType: 'side',
+    initialFacingDirection: 'left'
+})
